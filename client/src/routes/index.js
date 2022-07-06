1d0faf4b441e1b957748e70b912774c5dfae2902
danuts <matei1337@icloud.com>
2022-07-06 17:06:20 +0300

    cannot access login or register page if you logged in

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
-import Register from "../views/account/Register.vue";
+import Home from "@/views/Home.vue";
+import Register from "@/views/account/Register.vue";
+import Login from "@/views/account/Login.vue";
+import { isLoggedIn } from "@/services/apiConnector";
 
 const routes = [
     {
-        path: "/",
+        path: "/store",
         name: "home",
         component: Home,
         meta: {
@@ -21,11 +23,40 @@ const routes = [
             title: "Inregistrare cont nou @ 07INTERNET",
         },
     },
+    {
+        path: "/store/login",
+        name: "login",
+        component: Login,
+        meta: {
+            authRequired: false,
+            title: "Autentificare @ 07INTERNET",
+        },
+    },
 ];
 
 const router = createRouter({
     history: createWebHistory(),
     routes,
+});
+
+router.beforeEach(async (to, from, next) => {
+    if (to.meta.authRequired) {
+        if (await isLoggedIn()) {
+            next();
+        } else {
+            next({ name: "login" });
+        }
+    } else {
+        next();
+    }
+
+    if (
+        isLoggedIn() &&
+        to.path === "/store/login" &&
+        to.path === "/store/register"
+    ) {
+        return router.push("/store");
+    }
 });
 
 export default router;
