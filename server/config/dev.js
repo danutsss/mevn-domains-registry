bc4090643584bb1e141b54bfebfd67eac494ab44
danuts <matei1337@icloud.com>
2022-07-06 13:58:55 +0300

    removed config folder form repo

--- server/config/dev.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    MONGODB_URI:
-        "mongodb+srv://danuts:danuts_1337W@mevn-stack-dev.qpdry.mongodb.net/?retryWrites=true&w=majority",
-    JWT_SECRET: "0x7-services-srl-devenv-0x7",
-    JWT_EXPIRY: "2h",
-    PORT: 3000,
-};
--- server/config/key.js
+++
@@ -1,7 +0,0 @@
-if (process.env.NODE_ENV === "production") {
-    // export production configuration.
-    module.exports = require("./prod");
-} else {
-    // export development configuration.
-    module.exports = require("./dev");
-}
