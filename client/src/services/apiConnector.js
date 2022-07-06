7c63d4b284ea0ae7046ba7a20be4eefd14cdd733
danuts <matei1337@icloud.com>
2022-07-06 17:06:01 +0300

    function to check either a user is logged in or not.

--- client/src/services/apiConnector.js
+++ client/src/services/apiConnector.js
@@ -1,9 +1,13 @@
 import axios from "axios";
 
+export const isLoggedIn = () => {
+    return localStorage.getItem("jwtToken") !== null;
+};
+
 export default () => {
     return axios.create({
         baseURL: "http://localhost:3000",
-        withCredentials: false,
+        withCredentials: true,
         headers: {
             "Content-Type": "application/json",
             Accept: "application/json",
