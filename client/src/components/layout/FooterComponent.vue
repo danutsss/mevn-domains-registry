bfb816838a56500233661d3799316682f641f074
danuts <matei1337@icloud.com>
2022-07-22 16:21:26 +0300

    created footer and header.
    created footer and header layout component using Bulma, which are present on every page.

---
+++ client/src/components/layout/FooterComponent.vue
@@ -0,0 +1,12 @@
+<template>
+  <footer class="footer">
+    <div class="content has-text-centered">
+      <p>
+        <strong>vite app</strong> by
+        <a href="https://github.com/matei1337">Matei Nicolae - Daniel</a>. The
+        source code is licensed
+        <a href="https://opensource.org/licenses/ISC">ISC</a>
+      </p>
+    </div>
+  </footer>
+</template>
