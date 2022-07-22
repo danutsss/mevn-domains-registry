f2d2c0bc083ec0c49c1900732c2560b27dadb813
danuts <matei1337@icloud.com>
2022-07-22 16:23:12 +0300

    update for app.vue
    imported footer & header component to app.vue, also created router links for each page.

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,91 +1,32 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
+import FooterComponentVue from "@/components/layout/FooterComponent.vue";
+import HeaderComponentVue from "@/components/layout/HeaderComponent.vue";
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
+  <HeaderComponentVue>
+    <template #link>
+      <RouterLink to="/" class="navbar-item">inregistreaza</RouterLink>
+      <RouterLink to="/transfer" class="navbar-item">transfer</RouterLink>
+    </template>
+    <template #dropdown-link>
+      <RouterLink to="/about" class="navbar-item">despre</RouterLink>
+      <RouterLink to="/contact" class="navbar-item">contact</RouterLink>
+      <hr class="navbar-divider" />
+      <RouterLink to="/report" class="navbar-item"
+        >raporteaza o problema</RouterLink
+      >
+    </template>
+    <template #buttons-link>
+      <RouterLink to="/register" class="button is-primary">
+        <strong>inregistreaza</strong>
+      </RouterLink>
+      <RouterLink to="/login" class="button is-light">logare</RouterLink>
+    </template>
+  </HeaderComponentVue>
   <RouterView />
+  <FooterComponentVue />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+<style></style>
