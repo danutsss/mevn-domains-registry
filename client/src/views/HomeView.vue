a539d51f9738e3ff19e8bbdbebfe58b1f1a1f76b
danuts <matei1337@icloud.com>
2022-07-26 12:37:53 +0300

    removed the section from the homeview

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -15,46 +15,12 @@ onMounted(() => {
 });
 </script>
 <template>
-  <section class="hero is-medium">
-    <div class="hero-body">
-      <Particles
-        id="tsparticles"
-        :particles-init="particlesInit"
-        :particles-loaded="particlesLoaded"
-        url="./src/assets/particles.json"
-      />
-      <div class="main container">
-        <div class="left__container">
-          <h1 class="title bold">cauta domeniu</h1>
-          <h2 class="subtitle">
-            introduceti domeniul dorit si verificati-i disponibilitatea.
-          </h2>
-          <form action="" method="get">
-            <div class="field has-addons">
-              <div class="control">
-                <input
-                  class="input"
-                  type="text"
-                  placeholder="ex.: bunicutzii.ro"
-                />
-              </div>
-              <div class="control">
-                <a class="button is-primary">verifica</a>
-              </div>
-            </div>
-          </form>
-          <h2 class="small mt-2">
-            in momentul de fata se pot inregistra numai domenii cu extensie .ro
-          </h2>
-        </div>
-        <div class="right__container">
-          <img
-            src="../assets/img/svgs/domain_names.svg"
-            alt="domain names"
-            width="675"
-          />
-        </div>
-      </div>
-    </div>
+  <section class="particles">
+    <Particles
+      id="tsparticles"
+      :particles-init="particlesInit"
+      :particles-loaded="particlesLoaded"
+      url="./src/assets/particles.json"
+    />
   </section>
 </template>
