fbea99811f6ce2cd9a1ac945b70b64eca843aac6
danuts <matei1337@icloud.com>
2022-07-28 15:52:12 +0300

    added head section and features section.

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,26 +1,206 @@
-<script setup>
-import { onMounted } from "vue";
-import { loadFull } from "tsparticles";
-
-const particlesInit = async engine => {
-  await loadFull(engine);
-};
-
-const particlesLoaded = async container => {
-  console.log("Particles container loaded", container);
-};
-
-onMounted(() => {
-  console.log("homeview mounted.");
-});
-</script>
+<script setup></script>
 <template>
-  <section class="particles">
-    <Particles
-      id="tsparticles"
-      :particles-init="particlesInit"
-      :particles-loaded="particlesLoaded"
-      url="./src/assets/particles.json"
-    />
+  <section class="relative">
+    <div
+      class="container flex flex-col-reverse lg:flex-row items-center gap-12 mt-14 lg:mt-28"
+    >
+      <!-- content start -->
+      <div class="flex flex-1 flex-col items-center lg:items-start">
+        <h2
+          class="text-primary-500 font-semibold text-3xl md:text-4 lg:text-5xl text-center lg:text-left mb-6"
+        >
+          Your .ro domain at just
+          <span class="text-secondary-300">6.99€</span> per year!
+        </h2>
+        <p class="text-blue-grey-900 text-lg text-center lg:text-left mb-6">
+          We've made it so easy to purchase a custom domain address that we're
+          guaranteeing your satisfaction! Registering your .ro is often the
+          first step toward turning your website into your online home. And now
+          you can do it for an unbeatable price.
+        </p>
+        <div class="flex justify-center flex-wrap gap-6">
+          <button
+            type="button"
+            class="btn btn-purple hover:bg-white hover:text-black"
+          >
+            Get started
+          </button>
+        </div>
+      </div>
+      <!-- content end -->
+
+      <!-- image start -->
+      <div class="flex justify-center flex-1 mb-10 md:mb-16 lg:mb-0 z-10">
+        <img
+          class="w-5/6 h-5/6 sm:w-3/4 sm:h-3/4 md:w-full md:h-full"
+          src="src/assets/img/svgs/domain_search.svg"
+          alt="domain_search"
+        />
+      </div>
+      <!-- image end -->
+    </div>
+
+    <!-- rounded rectangle start -->
+    <div
+      class="hidden md:block overflow-hidden bg-secondary-100 rounded-l-full absolute h-80 w-2/4 top-32 right-0 lg: -bottom-28 lg:-right-36"
+    ></div>
+    <!-- rounded rectangle end -->
+  </section>
+
+  <!-- features start -->
+  <section class="bg-grey py-40 mt-20 lg:mt-60">
+    <!-- heading start -->
+    <div class="sm:w-3/4 lg:w-5/12 mx-auto px-2">
+      <h1 class="text-3xl text-center text-blue-grey-900">Features</h1>
+      <p class="text-center text-blue-grey-700 mt-4">
+        Our aim is to make it quick and easy for you to register your favourite
+        .ro domain.
+      </p>
+    </div>
+    <!-- heading end -->
+
+    <!-- feature #1 start -->
+    <div class="relative mt-20 lg:mt-24">
+      <div
+        class="container flex flex-col lg:flex-row items-center justify-center gap-x-24"
+      >
+        <!-- image start -->
+        <div class="flex flex-1 justify-center z-10 mb-10 lg:mb-0">
+          <img
+            class="w-5/6 h-5/6 sm:w-3/4 sm:h-3/4 md:w-full md:h-full"
+            src="src/assets/img/svgs/user_flow.svg"
+            alt="user_flow"
+          />
+        </div>
+        <!-- image end -->
+
+        <!-- content start -->
+        <div class="flex flex-1 flex-col items-center lg:items-start">
+          <h1 class="text-3xl text-blue-grey-900">User-friendly interface</h1>
+          <p
+            class="text-blue-grey-700 my-4 text-center lg:text-left sm:w-3/4 lg:w-full"
+          >
+            Our team puts a lot of effort and work in order to ensure that our
+            user interface is easy-to-use.
+          </p>
+        </div>
+        <!-- content end -->
+      </div>
+
+      <!-- rounded rectangle start-->
+      <div
+        class="hidden lg:block overflow-hidden bg-secondary-100 rounded-r-full absolute h-80 w-2/4 -bottom-24 -left-36"
+      ></div>
+      <!-- rounder rectangle end -->
+    </div>
+    <!-- feature #1 end -->
+
+    <!-- feature #2 start -->
+    <div class="relative mt-20 lg:mt-52">
+      <div
+        class="container flex flex-col lg:flex-row-reverse items-center justify-center gap-x-24"
+      >
+        <!-- image start -->
+        <div class="flex flex-1 justify-center z-10 mb-10 lg:mb-0">
+          <img
+            class="w-5/6 h-5/6 sm:w-3/4 sm:h-3/4 md:w-full md:h-full"
+            src="src/assets/img/svgs/affordable.svg"
+            alt="affordable"
+          />
+        </div>
+        <!-- image end -->
+
+        <!-- content start -->
+        <div class="flex flex-1 flex-col items-center lg:items-start">
+          <h1 class="text-3xl text-blue-grey-900">Affordable</h1>
+          <p
+            class="text-blue-grey-700 my-4 text-center lg:text-left sm:w-3/4 lg:w-full"
+          >
+            With us you get detailed information about your domains and the
+            price for registering is only 6.99€ per year.
+          </p>
+        </div>
+        <!-- content end -->
+      </div>
+
+      <!-- rounded rectangle start -->
+      <div
+        class="hidden lg:block overflow-hidden bg-secondary-100 rounded-l-full absolute h-80 w-2/4 -bottom-24 -right-36"
+      ></div>
+      <!-- rounded rectangle end -->
+    </div>
+    <!-- feature #2 end -->
+
+    <!-- feature #3 start -->
+    <div class="relative mt-20 lg:mt-52">
+      <div
+        class="container flex flex-col lg:flex-row items-center justify-center gap-x-24"
+      >
+        <!-- image start -->
+        <div class="flex flex-1 justify-center z-10 mb-10 lg:mb-0">
+          <img
+            class="w-5/6 h-5/6 sm:w-3/4 sm:h-3/4 md:w-full md:h-full"
+            src="src/assets/img/svgs/reliable.svg"
+            alt="reliable"
+          />
+        </div>
+        <!-- image end -->
+
+        <!-- content start -->
+        <div class="flex flex-1 flex-col items-center lg:items-start">
+          <h1 class="text-3xl text-blue-grey-900">Reliable service</h1>
+          <p
+            class="text-blue-grey-700 my-4 text-center lg:text-left sm:w-3/4 lg:w-full"
+          >
+            Everything you need is just one click away. Standard registration,
+            reservation, transfer, and more: choose the right one for you.
+          </p>
+        </div>
+        <!-- content end -->
+      </div>
+
+      <!-- rounded rectangle start -->
+      <div
+        class="hidden lg:block overflow-hidden bg-secondary-100 rounded-r-full absolute h-80 w-2/4 -bottom-24 -left-36"
+      ></div>
+      <!-- rounded rectangle end -->
+    </div>
+
+    <!-- feature #4 start -->
+    <div class="relative mt-20 lg:mt-52">
+      <div
+        class="container flex flex-col lg:flex-row-reverse items-center justify-center gap-x-24"
+      >
+        <!-- image start -->
+        <div class="flex flex-1 justify-center z-10 mb-10 lg:mb-0">
+          <img
+            class="w-5/6 h-5/6 sm:w-3/4 sm:h-3/4 md:w-full md:h-full"
+            src="src/assets/img/svgs/security.svg"
+            alt="secured"
+          />
+        </div>
+        <!-- image end -->
+
+        <!-- content start -->
+        <div class="flex flex-1 flex-col items-center lg:items-start">
+          <h1 class="text-3xl text-blue-grey-900">Secured</h1>
+          <p
+            class="text-blue-grey-700 my-4 text-center lg:text-left sm:w-3/4 lg:w-full"
+          >
+            Security on any level: we offer 12 months unconditional and
+            hassle-free coverage - this means a hassle-free purchase and
+            friendly customer experience.
+          </p>
+        </div>
+        <!-- content end -->
+      </div>
+
+      <!-- rounded rectangle start -->
+      <div
+        class="hidden lg:block overflow-hidden bg-secondary-100 rounded-l-full absolute h-80 w-2/4 -bottom-24 -right-36"
+      ></div>
+      <!-- rounded rectangle end -->
+    </div>
+    <!-- feature #4 end -->
   </section>
 </template>
