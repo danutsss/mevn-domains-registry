2e8451d16f624641ceafb4ce976a682d161d35c8
danuts <matei1337@icloud.com>
2022-07-29 16:07:39 +0300

    created a api helper for easy use.

---
+++ client/src/services/apiConnector.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+export const isLoggedIn = () => {
+  return !!localStorage.getItem("token");
+};
+
+export default () => {
+  return axios.create({
+    baseURL: "http://localhost:3000",
+    withCredentials: true,
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+  });
+};
+
+// export const ucrmApiRequest = (method, url, data) => {
+//   return axios({
+//     method: method,
+//     url: url,
+//     data: data,
+//     headers: {
+//       "Content-Type": "application/json",
+//       "X-Auth-App-Key": config.ucrmApiKey,
+//     },
+//   });
+// };
