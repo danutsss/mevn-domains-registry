4c507aa299f8ea7a1d62b7e1b293d3fbb4cfe231
danuts <matei1337@icloud.com>
2022-07-29 16:07:26 +0300

    now, if you click on the header logo, you'll be redirected to home page.

--- client/src/components/layout/HeaderComponent.vue
+++ client/src/components/layout/HeaderComponent.vue
@@ -49,8 +49,10 @@ document.addEventListener("DOMContentLoaded", () => {
               d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"
             />
           </svg>
-          <span class="text-blue-grey-900" style="margin-left: -40px"
-            >ZERO SAPTE SERVICES</span
+          <a href="/"
+            ><span class="text-blue-grey-900" style="margin-left: -40px"
+              >ZERO SAPTE SERVICES</span
+            ></a
           >
         </div>
 
