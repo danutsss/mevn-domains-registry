8c2264ba8458a554654548912c0d91cf91562370
danuts <matei1337@icloud.com>
2022-08-09 18:56:31 +0300

    added shopping cart icon which redirects to the shopping cart view page.

--- client/src/App.vue
+++ client/src/App.vue
@@ -21,6 +21,26 @@ const authStore = useAuthStore();
       <hr class="navbar-divider" />
       <RouterLink to="/report" class="navbar-item">Report a problem</RouterLink>
     </template>
+
+    <template #cart-icon>
+      <RouterLink to="/cart"
+        ><svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6 text-blue-grey-900"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          stroke-width="2"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+          />
+        </svg>
+      </RouterLink>
+    </template>
+
     <template v-if="!authStore.user" #buttons-link>
       <RouterLink
         to="/register"
@@ -31,6 +51,7 @@ const authStore = useAuthStore();
         >Login</RouterLink
       >
     </template>
+
     <template v-else #buttons-link>
       <RouterLink
         to="/dashboard"
