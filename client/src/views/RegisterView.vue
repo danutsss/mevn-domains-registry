33ee994c9dee1496ab14eadf6ef80ac222fcf2e1
danuts <matei1337@icloud.com>
2022-08-09 13:48:05 +0300

    design logic for register page done

--- client/src/views/RegisterView.vue
+++ client/src/views/RegisterView.vue
@@ -1,9 +1,14 @@
 <script setup>
 import { LockClosedIcon } from "@heroicons/vue/solid";
-import { ref } from "vue";
+import { onMounted, ref } from "vue";
 
 const firstName = ref();
 const lastName = ref();
+const personType = ref();
+
+onMounted(() => {
+  personType.value = "NOT_SELECTED";
+});
 </script>
 
 <template>
@@ -59,11 +64,11 @@ const lastName = ref();
             />
           </div>
           <div class="w-1/2">
-            <label for="lastName" class="sr-only">Last name</label>
+            <label for="last_name" class="sr-only">Last name</label>
             <input
-              id="lastName"
+              id="last_name"
               v-model="lastName"
-              name="lastName"
+              name="last_name"
               type="text"
               autocomplete="family-name"
               class="shadow-sm appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
@@ -71,6 +76,180 @@ const lastName = ref();
               required
             />
           </div>
+        </div>
+
+        <div class="rounded-md shadow-sm -space-y-px">
+          <div class="w-full">
+            <label for="person_type" class="sr-only">Person type</label>
+            <select
+              id="person_type"
+              v-model="personType"
+              name="person_type"
+              placeholder="Person type"
+              class="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              required
+            >
+              <option selected disabled value="NOT_SELECTED">
+                Select person type
+              </option>
+              <option value="p">Private person</option>
+              <option value="ap">Authorized person</option>
+              <option value="nc">Non-commercial organization</option>
+              <option value="c">Commercial organization</option>
+              <option value="gi">Government institute</option>
+              <option value="pi">Public institute</option>
+              <option value="o">Other</option>
+            </select>
+          </div>
+        </div>
+
+        <div class="rounded-md shadow-sm -space-y-px">
+          <div class="w-full">
+            <label for="cnp" class="sr-only"
+              >Fiscal code (for companies) or identification number (for
+              individuals)</label
+            >
+
+            <input
+              id="cnp"
+              v-model="cnp"
+              name="cnp"
+              type="text"
+              placeholder="Fiscal code (companies) or CNP (for individual)"
+              class="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              required
+            />
+          </div>
+        </div>
+
+        <div class="rounded-md shadow-sm -space-y-px">
+          <div class="w-full">
+            <label for="nr_reg_com" class="sr-only"
+              >Nr. Reg. Com (companies)</label
+            >
+            <input
+              id="nr_reg_com"
+              v-model="nr_reg_com"
+              class="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              name="nr_reg_com"
+              placeholder="Nr. Reg. Com. (companies)"
+              type="text"
+            />
+          </div>
+        </div>
+
+        <div class="rounded-md shadow-sm -space-y-px">
+          <div class="w-full">
+            <label for="email-address" class="sr-only">Email address</label>
+            <input
+              id="email-address"
+              v-model="email"
+              name="email"
+              type="email"
+              autocomplete="email"
+              class="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              placeholder="Email address"
+              required
+            />
+          </div>
+        </div>
+
+        <div class="flex flex-row rounded-md gap-2">
+          <div class="w-1/2">
+            <label for="password" class="sr-only">Password</label>
+            <input
+              id="password"
+              v-model="password"
+              name="password"
+              type="password"
+              autocomplete="password"
+              class="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              placeholder="Password"
+              required
+            />
+          </div>
+
+          <div class="w-1/2">
+            <label for="confirm_password" class="sr-only"
+              >Confirm password</label
+            >
+            <input
+              id="confirm_password"
+              v-model="confirm_password"
+              name="confirm_password"
+              type="confirm_password"
+              autocomplete="confirm_password"
+              class="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              placeholder="Confirm password"
+              required
+            />
+          </div>
+        </div>
+
+        <div class="rounded-md shadow-sm -space-y-px">
+          <div class="w-full">
+            <label for="phone_number" class="sr-only">Phone number</label>
+            <input
+              id="phone_number"
+              v-model="phone_number"
+              name="phone_number"
+              type="text"
+              class="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              placeholder="Phone number (mandatory format: +40.xxxxxxxxx)"
+              required
+            />
+          </div>
+        </div>
+
+        <div class="rounded-md shadow-sm -space-y-px">
+          <div class="w-full">
+            <label for="address" class="sr-only">Address</label>
+            <input
+              id="address"
+              v-model="address"
+              name="address"
+              type="text"
+              class="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              placeholder="Address"
+              required
+            />
+          </div>
+        </div>
+
+        <div class="flex flex-row rounded-md gap-2">
+          <div class="w-1/2">
+            <label for="city" class="sr-only">City</label>
+            <input
+              id="city"
+              v-model="city"
+              name="city"
+              type="city"
+              autocomplete="city"
+              class="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              placeholder="City"
+              required
+            />
+          </div>
+
+          <div class="w-1/2">
+            <label for="county" class="sr-only">County</label>
+            <input
+              id="county"
+              v-model="county"
+              name="county"
+              type="county"
+              autocomplete="county"
+              class="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              placeholder="County"
+              required
+            />
+          </div>
+        </div>
+
+        <div>
+          <p class="text-xs text-center text-gray-500">
+            By registering on our website, you agree to our Terms.
+          </p>
         </div>
 
         <div>
