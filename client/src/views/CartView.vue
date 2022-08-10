817fdf87d6827ad6d82ef1a41c12bcdf89e9b8a8
danuts <matei1337@icloud.com>
2022-08-10 15:52:27 +0300

    started working on cart's desgin and js logic

--- client/src/views/CartView.vue
+++ client/src/views/CartView.vue
@@ -1,10 +1,17 @@
 <script setup>
 import { useCartStore } from "@/stores";
+import { ref } from "vue";
 
-const getCart = () => {
-  const cartStore = useCartStore();
+const quantity = ref(1);
 
-  return cartStore.getCart();
+const cartObj = JSON.parse(localStorage.getItem("cart"));
+const cartItems = cartObj ? cartObj.items : [];
+// const cartTotal = cartObj ? cartObj.total : 0;
+
+const getCartTotal = () => {
+  return cartItems.reduce(acc => {
+    return acc + 6.99 * quantity.value;
+  }, 0);
 };
 
 const deleteCart = async () => {
@@ -12,6 +19,15 @@ const deleteCart = async () => {
 
   return await cartStore.clearCart();
 };
+
+// const increaseQuantity = async id => {
+//   console.log(`increaseQuantity: ${id}`);
+
+//   // update total
+//   const cartStore = useCartStore();
+
+//   return await cartStore.increaseQuantity(id);
+// };
 </script>
 
 <template>
@@ -19,7 +35,20 @@ const deleteCart = async () => {
     <div
       class="container flex flex-col-reverse lg:flex-row items-center gap-12 mt-14 lg:mt-28"
     >
-      {{ getCart() }}
+      <div v-for="(product, index) in cartItems" :key="product.id">
+        #{{ index + 1 }} - {{ product }}
+
+        <input
+          :id="product.id"
+          v-model="quantity"
+          type="number"
+          min="1"
+          max="10"
+        />
+      </div>
+      <div>
+        {{ getCartTotal().toFixed(2) }}
+      </div>
     </div>
   </section>
 
