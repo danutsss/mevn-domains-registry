6b50784eccde466c1cc5fafa36f7ec5b52b089fd
danuts <matei1337@icloud.com>
2022-08-12 16:10:54 +0300

    update to `cart.store.js`

--- client/src/stores/cart.store.js
+++ client/src/stores/cart.store.js
@@ -3,40 +3,32 @@ import { defineStore } from "pinia";
 export const useCartStore = defineStore({
   id: "cart",
   state: () => ({
-    items: [],
-    total: 0,
+    cart: JSON.parse(localStorage.getItem("cart")) || [],
   }),
-
   actions: {
-    addItem(item) {
-      // push cart into local storage
+    addToCart(item) {
       if (localStorage.getItem("cart")) {
         const cart = JSON.parse(localStorage.getItem("cart"));
-        cart.items.push(item);
-        cart.total += 6.99;
-        localStorage.setItem(
-          "cart",
-          JSON.stringify({ items: cart.items, total: cart.total }),
-        );
+        cart.push({ item, registerPeriod: 1 });
+        localStorage.setItem("cart", JSON.stringify(cart));
       } else {
         localStorage.setItem(
           "cart",
-          JSON.stringify({ items: [item], total: 6.99 }),
+          JSON.stringify([{ item, registerPeriod: 1 }]),
         );
       }
     },
 
-    removeItem(item) {
-      this.items.splice(this.items.indexOf(item), 1);
-      this.total -= 6.99;
-
-      // remove item from local storage
-      localStorage.setItem("cart", JSON.stringify(this.items));
+    getTotal() {
+      let total = 0;
+      this.cart.forEach(item => {
+        total += 6.99 * item.registerPeriod;
+      });
+      return total;
     },
 
     clearCart() {
-      this.items = [];
-      this.total = 0;
+      this.cart = [];
 
       // remove cart from local storage
       localStorage.removeItem("cart");
@@ -44,15 +36,5 @@ export const useCartStore = defineStore({
       // refresh page to show empty cart
       window.location.reload();
     },
-
-    increaseQuantity() {
-      this.total += 6.99;
-
-      // update total in localstorage
-    },
-  },
-
-  destroyed() {
-    localStorage.setItem("cart", JSON.stringify(this.items));
   },
 });
