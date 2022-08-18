9ded5e7ede3c7da6bf4628de6ffdf73c6c5e399f
danuts <matei1337@icloud.com>
2022-08-18 16:49:02 +0300

    route to get all user's domains and invoices

--- server/routes/domain.js
+++ server/routes/domain.js
@@ -4,5 +4,6 @@ const Domain = require("../controllers/domain");
 const router = express.Router();
 
 router.post("/create", Domain.create);
+router.get("/:id/all", Domain.showall);
 
 module.exports = router;
--- server/routes/invoice.js
+++ server/routes/invoice.js
@@ -4,5 +4,6 @@ const Invoice = require("../controllers/invoice");
 const router = express.Router();
 
 router.post("/create", Invoice.create);
+router.get("/:id/all", Invoice.showall);
 
 module.exports = router;
