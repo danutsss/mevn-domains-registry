ea75bbcfdab6396f70d1e1bb5bcdd03516ac82bd
danuts <matei1337@icloud.com>
2022-08-31 14:15:03 +0300

    came back to base view of the dashboard

--- client/src/views/Dashboard/DashboardView.vue
+++ client/src/views/Dashboard/DashboardView.vue
@@ -1,29 +1,5 @@
 <script setup>
-import apiConnector from "@/services/apiConnector";
-import { getUserFromLocalStorage, updateInvoices } from "@/services/helpers";
-import { onMounted, ref } from "vue";
-
-const domains = ref("");
-
-onMounted(() => {
-  apiConnector()
-    .get(`/api/domains/${getUserFromLocalStorage()["client"]["_id"]}/all`)
-    .then(res => {
-      for (let i = 0; i < res.data.domains.length; i++) {
-        setInterval(() => {
-          updateInvoices(res.data.domains[i].domainInvoiceId);
-        }, 100);
-      }
-      apiConnector()
-        .get(`/api/invoices/${getUserFromLocalStorage()["client"]["_id"]}/all`)
-        .then(res => {
-          console.log(res);
-          for (let i = 0; i < res.data.invoices.length; i++) {
-            domains.value = res.data.invoices[i].invoiceStatus;
-          }
-        });
-    });
-});
+import { getUserFromLocalStorage } from "@/services/helpers";
 </script>
 
 <template>
@@ -31,7 +7,7 @@ onMounted(() => {
     <div
       class="container flex flex-col-reverse lg:flex-row items-center gap-12 mt-14 lg:mt-28"
     >
-      {{ domains }}
+      {{ getUserFromLocalStorage() }}
     </div>
   </section>
 </template>
