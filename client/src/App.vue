3db0ef93bd47a976e4ec9c03fcbf8917b13ee2f0
danuts <matei1337@icloud.com>
2022-09-08 17:43:30 +0300

    added a dropdown menu into the navbar which has: dashboard, account settings and logout

--- client/src/App.vue
+++ client/src/App.vue
@@ -4,7 +4,11 @@ import FooterComponentVue from "@/components/layout/FooterComponent.vue";
 import HeaderComponentVue from "@/components/layout/HeaderComponent.vue";
 import { useAuthStore } from "@/stores";
 import { cartLength } from "@/services/helpers";
+import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/vue";
+import { ChevronDownIcon } from "@heroicons/vue/solid";
+import { getUserFromLocalStorage } from "@/services/helpers";
 
+const user = getUserFromLocalStorage();
 const authStore = useAuthStore();
 </script>
 
@@ -56,16 +60,78 @@ const authStore = useAuthStore();
     </template>
 
     <template v-else #buttons-link>
-      <RouterLink
-        to="/dashboard"
-        class="button bg-primary-500 text-white font-semibold"
-      >
-        Dashboard
-      </RouterLink>
+      <Menu as="div" class="relative inline-block text-left">
+        <div>
+          <MenuButton
+            class="button inline-flex justify-center w-full font-medium leading-5 text-blue-grey-700 transition duration-150 ease-in-out bg-white border border-blue-grey-300 rounded-md hover:text-blue-grey-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-blue-grey-50 active:text-blue-grey-800"
+          >
+            Options
+            <ChevronDownIcon
+              class="ml-2 -mr-1 h-5 w-5 text-blue-grey-700 hover:text-blue-grey-500"
+              aria-hidden="true"
+            />
+          </MenuButton>
+        </div>
 
-      <button class="button is-light font-semibold" @click="authStore.logout()">
-        Logout
-      </button>
+        <transition
+          enter-active-class="transition duration-100 ease-out"
+          enter-from-class="transform scale-95 opacity-0"
+          enter-to-class="transform scale-100 opacity-100"
+          leave-active-class="transition duration-75 ease-in"
+          leave-from-class="transform scale-100 opacity-100"
+          leave-to-class="transform scale-95 opacity-0"
+        >
+          <MenuItems
+            class="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+          >
+            <div className="px-4 py-3">
+              <p className="text-sm leading-5">Signed in as</p>
+              <p
+                className="text-sm font-medium leading-5 text-blue-grey-900 truncate"
+              >
+                {{ user["client"]["email"] }}
+              </p>
+            </div>
+            <div class="px-1 py-1">
+              <MenuItem v-slot="{ active }">
+                <RouterLink
+                  to="/dashboard"
+                  :class="[
+                    active ? 'bg-primary-500 text-white' : 'text-blue-grey-900',
+                    'group flex w-full items-center rounded-md px-2 py-2 text-sm',
+                  ]"
+                  >Dashboard</RouterLink
+                >
+              </MenuItem>
+              <MenuItem v-slot="{ active }">
+                <RouterLink
+                  to="/dashboard/profile"
+                  :class="[
+                    active ? 'bg-primary-500 text-white' : 'text-blue-grey-900',
+                    'group flex w-full items-center rounded-md px-2 py-2 text-sm',
+                  ]"
+                >
+                  Account settings
+                </RouterLink>
+              </MenuItem>
+            </div>
+
+            <div class="px-1 py-1">
+              <MenuItem v-slot="{ active }">
+                <button
+                  :class="[
+                    active ? 'bg-primary-500 text-white' : 'text-blue-grey-900',
+                    'group flex w-full items-center rounded-md px-2 py-2 text-sm',
+                  ]"
+                  @click="authStore.logout()"
+                >
+                  Logout
+                </button>
+              </MenuItem>
+            </div>
+          </MenuItems>
+        </transition>
+      </Menu>
     </template>
   </HeaderComponentVue>
   <RouterView />
