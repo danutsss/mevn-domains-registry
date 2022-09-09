dced98ffce0f75eff23f595cd65dfb5fb347c4ee
danuts <matei1337@icloud.com>
2022-09-09 15:09:02 +0300

    solved design bugs on profile page

--- client/src/views/Dashboard/Account/ProfileView.vue
+++ client/src/views/Dashboard/Account/ProfileView.vue
@@ -23,16 +23,18 @@ const personType = () => {
 };
 </script>
 <template>
-  <section class="relative mt-14 lg:mt-28">
-    <div class="container pt-3 pb-5 d-flex justify-start align-items-center">
-      <span class="display-4 fw-bold me-3"
+  <section class="relative mt-28 mb-28">
+    <div
+      class="container pt-3 pb-5 flex flex-col lg:flex-row justify-start items-center"
+    >
+      <span class="text-5xl font-bold mr-3"
         >{{ user.last_name }} {{ user.first_name }}</span
       ><span
-        class="border-success border-2 text-success fw-bold px-3 py-2 rounded"
+        class="border-secondary-500 border-2 text-secondary-500 font-bold p-3 rounded w-[150px]"
         >{{ personType() }}</span
       >
     </div>
-    <div class="container grid grid-cols-3 gap-4">
+    <div class="container flex flex-col lg:grid lg:grid-cols-3 gap-4">
       <div id="profile__container">
         <div class="bg-white p-5 shadow-lg rounded-3xl">
           <div id="profile__image">
@@ -41,21 +43,17 @@ const personType = () => {
               class="rounded-full bg-primary-200 h-[120px] w-[120px] mx-auto"
             />
           </div>
-          <div id="profile__name" class="mt-4">
-            <h1 class="text-2xl text-center mb-2 font-semibold text-gray-700">
-              {{ user.first_name }}
-            </h1>
-            <h2 class="text-xl font-semibold text-gray-500">
-              {{ user.email }}
-            </h2>
+
+          <div id="profile__information" class="flex flex-row justify-center">
+            <span class="text-gray-500 text-2xl font-bold mt-3 mb-3">
+              {{ user.email }}</span
+            >
           </div>
         </div>
       </div>
       <div class="col-span-2">
         <div id="profile__information">
-          <div
-            class="bg-primary-500 p-20 border-1 border-black shadow-lg rounded-lg"
-          ></div>
+          <div class="bg-white p-20 shadow-lg rounded-3xl"></div>
         </div>
       </div>
     </div>
